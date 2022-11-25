6772999091a544110b3d210404e5adcb8758f624
jeongsardine <jeong_eo_ri@snu.ac.kr>
2022-11-25 12:54:54 +0900

    commit

---
+++ hw-5/CMakeFiles/01_05_new_mesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_05_new_mesh"
+  "01_05_new_mesh.pdb"
+  "CMakeFiles/01_05_new_mesh.dir/01_05_new_mesh.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_05_new_mesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
